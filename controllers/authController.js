e7d22d8b6c1c3e0661c670e3d715bd41c8c41125
MEKL20 <edwinkhoirudin16@gmail.com>
2022-01-18 17:06:39 +0700

    Create Register adn Login

---
+++ controllers/authController.js
@@ -0,0 +1,112 @@
+const joi = require("joi");
+const { User } = require("../models");
+const { hashPassword, comparePassword } = require("../utils/bcrypt");
+const { generateToken } = require("../utils/jwt");
+const errorHandler = require("../utils/error-handler");
+
+module.exports = {
+  register: async (req, res) => {
+    const { firstName, lastName, email, password } = req.body;
+    try {
+      const schema = joi.object({
+        firstName: joi.string().required(),
+        lastName: joi.string().required(),
+        email: joi.string().email().required(),
+        password: joi.string().required(),
+      });
+
+      const { error } = schema.validate(req.body);
+      if (error) {
+        return res.status(400).json({
+          message: error.message,
+          status: "Bad Request",
+        });
+      }
+
+      const check = await User.findOne({
+        where: { email },
+      });
+
+      if (check) {
+        return res.status(400).json({
+          message: "Email is Already in Use",
+          status: "Bad Request",
+        });
+      }
+
+      const passwordHashed = hashPassword(password);
+
+      const user = await User.create({
+        firstName,
+        lastName,
+        email,
+        password: passwordHashed,
+      });
+
+      const token = generateToken({
+        id: user.id,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        email: user.email,
+      });
+
+      res.status(200).json({
+        message: "Register Success",
+        status: "OK",
+        result: { token },
+      });
+    } catch (error) {
+      errorHandler(res, error);
+    }
+  },
+
+  login: async (req, res) => {
+    const { email, password } = req.body;
+    try {
+      const schema = joi.object({
+        email: joi.string().email().required(),
+        password: joi.string().required(),
+      });
+
+      const { error } = schema.validate(req.body);
+      if (error) {
+        return res.status(400).json({
+          message: error.message,
+          status: "Bad Request",
+        });
+      }
+
+      const user = await User.findOne({
+        where: { email },
+      });
+
+      if (!user) {
+        return res.status(401).json({
+          message: "Username or Password Incorrect",
+          status: "Unauthorized",
+        });
+      }
+
+      const checkValid = comparePassword(password, user.password);
+      if (!checkValid) {
+        return res.status(401).json({
+          message: "Username or Password Incorrect",
+          status: "Unauthorized",
+        });
+      }
+
+      const token = generateToken({
+        id: user.id,
+        email: user.email,
+      });
+
+      res.status(200).json({
+        message: "Login Success",
+        status: "OK",
+        result: { token },
+      });
+    } catch (error) {
+      errorHandler(res, error);
+    }
+  },
+};
