9c106eb5766b7aa40ec3e0c200c3985bbdaded33
Juannjuu <juanrahmadhika@gmail.com>
2022-01-18 10:55:59 +0700

    ! update routes and package

--- routes/auth-route.js
+++ routes/auth-route.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const {} = require("../controllers/authController")
 
 
 module.exports = router
--- routes/event-route.js
+++ routes/event-route.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const {} = require("../controllers/eventController")
 
 
 module.exports = router
