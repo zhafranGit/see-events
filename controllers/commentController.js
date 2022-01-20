0cfe5441a9fd8a493af7b2d5e9d71a8c4b4c6aca
MEKL20 <edwinkhoirudin16@gmail.com>
2022-01-20 23:59:13 +0700

    fix post comment userId sudah bisa auto

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -1,13 +1,15 @@
 const errorHandler = require('../utils/error-handler') //error handler
 const { Comment, Event } = require('../models') // use models
+const { verifyToken } = require("../utils/jwt")
 
 
 module.exports = {
  createComment: async (req, res) => {
     const body = req.body;
     const {eventId: id} = req.params
+    const userId = verifyToken(req.headers.authorization.replace("Bearer ", ""));
     try {
-        const comment = await Comment.create({...body, eventId: id})
+        const comment = await Comment.create({...body, eventId: id, userId: userId.id})
         //user id masih harus input manual di body
         if (!comment) {
             return res.status(500).json({
