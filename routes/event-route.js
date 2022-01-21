5f72dcc9db060b4b30ffe766934c217222e96151
MEKL20 <edwinkhoirudin16@gmail.com>
2022-01-21 22:24:48 +0700

    + isLogin di post comment

--- routes/event-route.js
+++ routes/event-route.js
@@ -5,9 +5,10 @@ const {
     getEventDetail,
     getEvents
 } = require("../controllers/eventController")
+const { isLogin } = require("../middlewares/auth")
 
 router.get("/", getEvents)
 router.get("/:eventId", getEventDetail)
-router.post("/:eventId/comment", createComment)
+router.post("/:eventId/comment", isLogin, createComment)
 
 module.exports = router
