97d4116fef7f224dada6271a8f50dbcf6813ee7e
Zhafran Saloom <zhafransaloom25@gmail.com>
2022-01-23 17:13:02 +0800

    + fix

--- app.js
+++ app.js
@@ -4,9 +4,15 @@ const routes = require('./routes');
 const port = process.env.PORT
 
 app.use(express.json());
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({ extended: false }))
 
 app.use('/api/v1', routes);
+
+app.get('/', (req, res) => {
+
+    res.send('hello from simple server :)')
+
+})
 
 app.listen(port, () => {
     console.log('App is running on port ', port);
