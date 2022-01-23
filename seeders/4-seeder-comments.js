45fbb1bb701d64b9dae058b455b4905669dd69ba
Zhafran Saloom <zhafransaloom25@gmail.com>
2022-01-23 17:34:00 +0800

    + fix v5

--- seeders/4-seeder-comments.js
+++ seeders/4-seeder-comments.js
@@ -4,25 +4,25 @@ module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.bulkInsert("Comments",
       [{
-          eventId: 21,
-          userId: 2,
-          comment: "mantap bang lanjutkan"
-        },
-        {
-          eventId: 21,
-          userId: 2,
-          comment: "ini komen dari user 2"
-        },
-        {
-          eventId: 22,
-          userId: 1,
-          comment: "ini komen dari user 1"
-        },
-        {
-          eventId: 22,
-          userId: 2,
-          comment: "mantap bang lanjutkan"
-        },
+        eventId: 1,
+        userId: 2,
+        comment: "mantap bang lanjutkan"
+      },
+      {
+        eventId: 2,
+        userId: 2,
+        comment: "ini komen dari user 2"
+      },
+      {
+        eventId: 3,
+        userId: 1,
+        comment: "ini komen dari user 1"
+      },
+      {
+        eventId: 4,
+        userId: 2,
+        comment: "mantap bang lanjutkan"
+      },
       ])
   },
 
